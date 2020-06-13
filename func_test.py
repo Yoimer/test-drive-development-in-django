be84b742447eab15c625e696735674f633c7f87f
Yoimer Román <ydrf19@gmail.com>
2020-06-12 22:17:02 -0400

    moved geckodriver.exe to C:\Users\username\AppData\Local\Programs\Python\Python36\Scripts making it global

--- func_test.py
+++ func_test.py
@@ -1,3 +1,6 @@
+#  geckodriver.exe has to be on the global path for python access to it.
+# the path is C:\Users\username\AppData\Local\Programs\Python\Python36\Scripts
+
 from selenium import webdriver
 
 browser = webdriver.Firefox()
